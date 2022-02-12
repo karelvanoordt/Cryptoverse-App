b1afe9e5a551256c67f80b6150c4b96b090370a1
karelvanoordt <63868555+karelvanoordt@users.noreply.github.com>
2022-02-12 15:51:45 -0500

    Add menu items

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Button, Menu, Typography, Avatar } from 'antd';
 import { Link } from 'react-router-dom';
-import { HomeOutlined, BulbOutlined, FundOutliuned, MenuOutlined } from '@ant-design/icons';
+import { HomeOutlined, BulbOutlined, FundOutlined, MenuOutlined, MoneyCollectOutlined } from '@ant-design/icons';
 import icon from '../images/cryptocurrency.png'
 
 const Navbar = () => {
@@ -12,10 +12,25 @@ const Navbar = () => {
             <Typography.Title level={2} className='logo'>
                 <Link to="/">Cryptoverse</Link>
             </Typography.Title>
-            {/* <Button className='menu-control-container'>
-                §
-            </Button> */}
         </div> 
+
+        <Menu theme='dark'>
+            <Menu.Item icon={<HomeOutlined />}>
+                <Link to="/">Home</Link>
+            </Menu.Item>
+
+            <Menu.Item icon={<FundOutlined />}>
+                <Link to="/cryptocurrencies">Cryptocurrencies</Link>
+            </Menu.Item>
+
+            <Menu.Item icon={<MoneyCollectOutlined />}>
+                <Link to="/exchanges">Exchanges</Link>
+            </Menu.Item>
+
+            <Menu.Item icon={<BulbOutlined />}>
+                <Link to="/news">News</Link>
+            </Menu.Item>
+        </Menu>
     </div>
   )
 }

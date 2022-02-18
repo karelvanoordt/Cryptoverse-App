eb275f1a2fea15c7ad0ac39c77ec9f6feb71a640
Karel van Oordt <vanoordtkarel@gmail.com>
2022-02-18 13:47:35 -0500

    Setup API

---
+++ src/services/cryptoApi.js
@@ -0,0 +1,24 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+
+const cryptoApiHeaders = {
+    'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
+    'x-rapidapi-key': '44ebf2ddf8msh571d26131656751p19f6e1jsn0f876e530388'
+};
+
+const baseUrl = 'https://coinranking1.p.rapidapi.com/coins';
+
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders });
+
+export const cryptoApi = createApi({
+    reducerPath: 'cryptoApi',
+    baseQuery: fetchBaseQuery({ baseUrl }),
+    endpoints: (builder) => ({
+        getCryptos: builder.query({
+            query: () => createRequest('/coins')
+        })
+    })
+});
+
+export const {
+    useGetCryptosQuery,
+} = cryptoApi;
